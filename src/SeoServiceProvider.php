326a3a3c50fc8822a46e52c81f28089d000f28eb
Mehedi Hasan <44167334+ProgrammerHasan@users.noreply.github.com>
2026-05-15 21:59:08 +0600

    :rocket: programmerhasan/seo

--- src/SeoServiceProvider.php
+++ src/SeoServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace ProgrammerHasan\Seo;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use ProgrammerHasan\Seo\Commands\AuditCommand;
 use ProgrammerHasan\Seo\Commands\InstallCommand;
@@ -34,10 +35,19 @@ final class SeoServiceProvider extends ServiceProvider
         ], 'seo-views');
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'seo');
+
         $this->loadViewComponentsAs('seo', [Meta::class]);
 
+        // Supports: <x-seo />
+        Blade::component('seo', Meta::class);
+
         if ($this->app->runningInConsole()) {
-            $this->commands([InstallCommand::class, SitemapCommand::class, RobotsCommand::class, AuditCommand::class]);
+            $this->commands([
+                InstallCommand::class,
+                SitemapCommand::class,
+                RobotsCommand::class,
+                AuditCommand::class,
+            ]);
         }
     }
 }
