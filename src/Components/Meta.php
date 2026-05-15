5619e6680a5c143a44341cb29833082e074a7690
Mehedi Hasan <44167334+ProgrammerHasan@users.noreply.github.com>
2026-05-15 21:29:36 +0600

    fix: satisfy phpstan for blade component and package trait

--- src/Components/Meta.php
+++ src/Components/Meta.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace ProgrammerHasan\Seo\Components;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 use ProgrammerHasan\Seo\Facades\Seo;
 
 final class Meta extends Component
 {
-    public function render()
+    public function render(): View
     {
-        return view('seo::components.meta', ['html' => Seo::toHtml()]);
+        /** @var view-string $view */
+        $view = 'seo::components.meta';
+
+        return view($view, [
+            'html' => Seo::toHtml(),
+        ]);
     }
 }
